d86cd8a366722ca81e0f9cfbdd84187b5178722f
ranggarmaste <ranggarmaste@gmail.com>
2017-04-02 20:54:58 +0700

    Add static, add base html, add index as sample

--- shop/views.py
+++ shop/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the shop index.")
+    template_name = 'shop/index.html'
+    return render(request, template_name)
