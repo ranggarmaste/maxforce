7d386ba3854f3971c54cea18fd4c7e7b8e66907f
ranggarmaste <ranggarmaste@gmail.com>
2017-04-02 20:32:11 +0700

    Add models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article, Product, City, ProductOrder
+
+admin.site.register(Article)
+admin.site.register(Product)
+admin.site.register(City)
+admin.site.register(ProductOrder)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+    sold = models.IntegerField(default=0)
+    description = models.CharField(max_length=200)
+    url_photo = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    delivery_price = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ProductOrder(models.Model):
+    name = models.CharField(max_length=50)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    email = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
