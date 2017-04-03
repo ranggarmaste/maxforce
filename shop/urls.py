fd8f27ce178d04126a1ef9d7510fb27a0db36b92
ranggarmaste <ranggarmaste@gmail.com>
2017-04-03 11:21:43 +0700

    Add buyer templates

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'shop'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^products/$', views.products, name='products'),
+    url(r'^products/(?P<pk>[0-9]+)/$', views.product, name='product'),
+    url(r'^products/(?P<pk>[0-9]+)/confirm$', views.confirm, name='confirm'),
+    url(r'^products/(?P<pk>[0-9]+)/buy$', views.buy, name='buy'),
+    url(r'^thanks$', views.thanks, name='thanks')
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,22 @@ from django.http import HttpResponse
 def index(request):
     template_name = 'shop/index.html'
     return render(request, template_name)
+
+def products(request):
+    template_name = 'shop/products.html'
+    return render(request, template_name)
+
+def product(request, pk):
+    template_name = 'shop/product.html'
+    return render(request, template_name)
+
+def confirm(request, pk):
+    template_name = 'shop/confirm.html'
+    return render(request, template_name)
+
+def buy(request, pk):
+    return HttpResponse("Ini harusnya proses form pembelian")
+
+def thanks(request):
+    template_name = 'shop/thanks.html'
+    return render(request, template_name)
