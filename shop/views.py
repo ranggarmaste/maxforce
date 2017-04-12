76bdfad249776e85660d24af75c4e838e178d364
ranggarmaste <ranggarmaste@gmail.com>
2017-04-12 18:52:27 +0700

    Show FB and IG feeds correctly

--- shop/views.py
+++ shop/views.py
@@ -36,15 +36,12 @@ def get_facebook():
 def index(request):
     template_name = 'shop/index.html'
     latest_products = Product.objects.all().order_by('-created_at')[:3]
-    # about = About.objects.all()[:1]
-    # for a in about:
-    #   {{ a.description }}
     latest_articles = Article.objects.all().order_by('-created_at')[:3]
-    # api = InstagramAPI(access_token=ig_access_token, client_secret=ig_client_secret)
-    # recent_media, next_ = api.user_recent_media(user_id="239869696", count=10)
-    # facebook_data = get_facebook()
-    # return render(request, template_name, {'recent_media' : recent_media, 'facebook_data' : facebook_data, 'latest_products' : latest_products, 'latest_articles' : latest_articles })
-    return render(request, template_name, {'latest_products' : latest_products, 'latest_articles' : latest_articles })
+    api = InstagramAPI(access_token=ig_access_token, client_secret=ig_client_secret)
+    recent_media, next_ = api.user_recent_media(user_id="239869696", count=10)
+    print(recent_media[0].__dict__)
+    facebook_data = get_facebook()
+    return render(request, template_name, {'recent_media' : recent_media, 'facebook_data' : facebook_data, 'latest_products' : latest_products, 'latest_articles' : latest_articles })
 
 def about(request):
     template_name = 'shop/about.html'
@@ -214,7 +211,6 @@ def admin_profile(request):
             form.save()
             return redirect(reverse('shop:about'))
         return render(request, template_name)
-    
+
     form = AboutForm(instance=profile)
     return render(request, template_name, { 'form' : form, 'about' : about})
-
